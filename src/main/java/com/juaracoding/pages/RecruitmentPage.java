54f50338cdd54a392b63333c1d9b0ed7eda7762e
jordycris <jordyholland86@gmail.com>
2024-02-29 23:49:14 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/pages/RecruitmentPage.java
@@ -0,0 +1,28 @@
+package com.juaracoding.pages;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RecruitmentPage {
+
+    private WebDriver driver;
+
+    public RecruitmentPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    // Locator Find Element menggunakan PageFactory
+    @FindBy(xpath = "//span[normalize-space()='Recruitment']")
+    public WebElement menuRecruitment;
+
+    @FindBy(xpath = "//button[normalize-space()='Add']")
+    public WebElement btnAddCandidate;
+
+    // Lanjutkan code
+
+
+}
